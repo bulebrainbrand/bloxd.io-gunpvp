909167c758dc05a20f0d941c0503e7c34262af7f
bulebrainbrand <h.yotsumoto0509@gmail.com>
2025-11-29 17:16:49 +0900

    Implement team win/lose messaging and player counts
    Added functions to handle winning and losing messages, set right info, and count team players.

--- worldcode.js
+++ worldcode.js
@@ -33,6 +33,9 @@ const startBattle = () => {
   teamData.forEach((team,id) => setTeam(id,team))
   redTicket = 400
   blueTicket = 400
+  for(const pos of beaconPos){
+    api.setBlock(pos,"Beacon")
+  }
 }
 
 const setTeam = (id,team) => {
@@ -83,13 +86,52 @@ const checkEnd = () => {
 }
 
 const end = (loseTeam) => {
-  
+  for(const id of api.getPlayerIds()){
+    api.setPosition(id,lobbySpawn)
+  }
+  for(const [id,team] of Array.from(teamData)){
+    if(team === loseTeam){lose(id)}else{win(id)};
+  }
+  teamData.clear()
+}
+
+const lose = id => {
+  api.sendMessage(id,"敗北")
+}
+
+const win = id => {
+  api.sendMessage(id,"勝利")
 }
 
 const downTicket = (team,amount) => {
   if(team === 0)redTicket -= amount
   if(team === 1)blueTicket -= amount
   api.broadcastMessage([{str:team === 0?"赤チームのチケット":"青チームのチケット",style:{color:team === 0?"red":"blue"}},`が${amount}減少し,${team === 0?redTicket:blueTicket}になりました`])
+}
+
+const setRightInfo = id => {
+  let text = []
+  if(phase === 0){
+    //あとでやる
+  }
+  if(phase === 1){
+    //あとでやる
+  }
+  api.setClientOption(id,"RightInfoText",text)
+}
+
+const getTeamPlayerAmount = () => {
+  let redAmount = 0
+  let blueAmount = 0
+  for(const team of Array.from(teamData.values())){
+    if(team === 0){
+      redAmount++
+    }
+    else{
+      blueAmount++
+    }
+  }
+  return [redAmount,blueAmount]
 }
 
 onPlayerDamagingOtherPlayer = (ap,dp) => {
@@ -109,5 +151,21 @@ onPlayerChangeBlock = (id,x,y,z,from,to) => {
   if(to === "Beacon"){
     const team = teamData.get(id)
     downTicket(Number(!team),10) //壊した人の逆のチームにする
+    return "preventChange"
+  }
+}
+
+onPlayerJoin = id => {
+  api.setPosition(id,lobbySpawn)
+  if(phase === 1){
+    const [redAmount,blueAmount] = getTeamPlayerAmount()
+    if(red < blueAmount){
+      teamData.set(id,0)
+      setTeam(id,0)
+    }
+    else{
+      teamData.set(id,1)
+      setTeam(id,1)
+    }
   }
 }
